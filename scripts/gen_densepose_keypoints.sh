9a3a7d28e6c0459d1963c248b16626208cb92c15
goodmattg <mattgoodman13@gmail.com>
2020-04-26 20:28:17 -0500

    working towards densepose keypoints generation

---
+++ scripts/gen_densepose_keypoints.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+IN_DIR=$1
+OUT_DIR=$2
+echo "Frame directory: $PWD/$IN_DIR"
+echo "Output directory: $PWD/$OUT_DIR"
+
+mkdir -p $OUT_DIR/render
+mkdir -p $OUT_DIR/keypoints
+
+# Spin up OpenPose container named 'pose'
+nvidia-docker run -d \
+  -it \
+  --name dense \
+  --net=host \
+  -e DISPLAY \
+  --runtime=nvidia \
+  --mount type=bind,source=$PWD/$IN_DIR,target=/data \
+  --mount type=bind,source=$PWD/$OUT_DIR,target=/out \
+  exsidius/openpose:openpose
+
+# OpenPose container must be named 'pose' for this to work
+OPENPOSE_CONTAINER_ID=$(docker ps -aqf "name=dense")
+echo "Spun up DensePose: $OPENPOSE_CONTAINER_ID"
+
+
+
+nvidia-docker run -v $DENSEPOSE/DensePoseData:/denseposedata -it densepose:c2-cuda9-cudnn7-wtsdata \
+python2 tools/infer.py \
+    --im $IN_DIR/frame_000001.png \
+    --output-dir $OUT_DIR/render \
+    wts/DensePose_ResNet101_FPN_s1x-e2e.pkl configs/DensePose_ResNet101_FPN_s1x-e2e.yaml
+    

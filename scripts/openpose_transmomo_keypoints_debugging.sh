d68ab7a798f4dfb845ea8c91f753c2822ec99bb6
goodmattg <mattgoodman13@gmail.com>
2020-05-23 21:23:43 -0500

    redoing keypoint generation and frame saving

--- scripts/openpose_transmomo_keypoints_debugging.sh
+++ scripts/openpose_transmomo_keypoints_debugging.sh
@@ -16,7 +16,9 @@ echo "Generating keypoints for: $IN_VIDEO"
 echo "Temporarory store for keypoints: $OUT_PATH"
 echo "Saving keypoints to: $OUT_KEYPOINT_NPY"
 
-mkdir $PWD/$OUT_PATH
+mkdir -p $PWD/$OUT_PATH/keypoints
+mkdir -p $PWD/$OUT_PATH/frames
+
 
 # Spin up OpenPose container named 'pose'
 docker run -d \
@@ -41,8 +43,8 @@ docker exec -it $OPENPOSE_CONTAINER_ID \
     --display 0 \
     --render_pose 0 \
     --write_video /out/$OUT_KEYPOINT_AVI \
-    --write_json /out \
-    --write_images /out
+    --write_json /out/keypoints \
+    --write_images /out/frames
 
 # Kill and remove the OpenPose Container
 docker kill $OPENPOSE_CONTAINER_ID
@@ -50,7 +52,8 @@ docker container rm $OPENPOSE_CONTAINER_ID
 
 # Bundle OpenPose keypoints to TransMoMo format (15, 2, seq_length) .npy file
 python scripts/bundle_openpose.py \
-  --keypoint-dir $PWD/$OUT_PATH \
+  --keypoint-dir $PWD/$OUT_PATH/keypoints \
+  --keypoint-base $OUT_BASENAME \
   --output-fname $OUT_KEYPOINT_NPY
 
 # mv $PWD/$OUT_PATH/$OUT_KEYPOINT_NPY .
